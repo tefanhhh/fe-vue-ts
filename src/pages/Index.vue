05b3827a13af80917db56a324b8a97c9d7d698df
Tefan Haetami <tefanhaetami87@gmail.com>
2022-11-20 16:52:31 +0700

    feat: add login page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,3 +1,25 @@
 <template>
-  <div>this is home</div>
+  <form class="form__login d-flex flex-column align-center justify-center">
+    <v-card width="300">
+      <v-card-title class="text-center">Login</v-card-title>
+      <div class="px-4 pb-4">
+        <v-text-field
+          variant="outlined"
+          label="username"
+          placeholder="input username..."
+          hide-details />
+        <br />
+        <v-text-field
+          variant="outlined"
+          label="password"
+          placeholder="input password..."
+          hide-details />
+      </div>
+    </v-card>
+  </form>
 </template>
+<style scoped>
+  .form__login {
+    height: calc(100vh - 48px);
+  }
+</style>
