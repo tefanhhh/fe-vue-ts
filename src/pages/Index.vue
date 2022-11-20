4380557e1448b8104c55c0d723ecc31c539a27eb
Tefan Haetami <tefanhaetami87@gmail.com>
2022-11-20 18:34:31 +0700

    chore: implementing login method (on progress)

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
   import { ref } from 'vue'
+  import { useAuthStore } from '../stores/auth'
+
+  const store = useAuthStore()
+  const { login } = store
+
   const [username, password] = [ref(''), ref('')]
   const showPassword = ref(false)
 
   function toggleShowPassword() {
     showPassword.value = !showPassword.value
   }
+
+  function _login(ev: Event) {
+    console.log(ev)
+    login('hai tefan')
+  }
 </script>
 <template>
-  <form class="form__login d-flex flex-column align-center justify-center">
+  <form
+    class="form__login d-flex flex-column align-center justify-center"
+    novalidate
+    @submit.prevent="_login">
     <v-card width="300">
       <v-card-title class="text-center">Login</v-card-title>
       <div class="px-4 pb-4">
@@ -29,6 +42,16 @@
           :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
           @click:append-inner="toggleShowPassword()" />
       </div>
+      <v-card-actions>
+        <v-btn
+          type="submit"
+          block
+          variant="flat"
+          color="primary"
+          class="text-capitalize font-weight-bold"
+          >Login</v-btn
+        >
+      </v-card-actions>
     </v-card>
   </form>
 </template>
