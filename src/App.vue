a5644e3da6b088190140fe3740bab0ec8ff69bd0
Tefan Haetami <tefanhaetami87@gmail.com>
2022-11-23 08:33:21 +0700

    chore: setting up unit test (citest)

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
   import { onMounted } from 'vue'
   import { useStore } from './stores'
   import Nav from './components/nav/Index.vue'
-  import countryApi from './apis/country'
+  import api from './apis'
 
   const store = useStore()
   const { setPageTitle } = store
 
   onMounted(() => {
     setPageTitle('fe vue ts')
-    countryApi.query().then(res => {
+    api.country.query().then(res => {
       console.log(res)
     })
   })
---
+++ src/apis/index.ts
@@ -0,0 +1,5 @@
+import country from './country'
+
+export default {
+  country,
+}
